666db56c960179b3f0e8614ee53fbb1c5d18e3a4
Yannik Wassmuth <wassmuth@students.uni-marburg.de>
2024-01-30 22:21:20 +0100

    update

--- persistence_calculator/FaceMapGenerator.py
+++ persistence_calculator/FaceMapGenerator.py
@@ -1,10 +1,50 @@
-from concurrent.futures import ThreadPoolExecutor
-from sage.all import Graph, identity_matrix
+from multiprocessing import Pool
+from sage.all import Matrix, ZZ
+from sage.matrix.special import block_matrix
 
 
-def generate_face_map(adjacency_graph, max_dim: int, start_options: list):
+def generate_face_maps(singular_cubes, start, stop):
+    result = [None] * stop[0]
+    if stop[1] > 0:
+        result.append(None)
 
-    return None
+    for cube_dim in range(start[0], stop[0] + 1):
+        start_index = start[1] if cube_dim == start[0] else 0
+        stop_index = stop[1] if cube_dim == stop[0] else len(singular_cubes[cube_dim])
+
+        if cube_dim == 0:
+            continue
+
+        face_matrix = [None for _ in range(start_index, stop_index)]
+        for j in range(start_index, stop_index):
+            # we need to generate the associated row in the result matrix, which is a column of face_matrix
+            matrix_column = [0] * len(singular_cubes[cube_dim - 1])  # initialize coöumn
+            summand = -1  # summand fluctuating between -1 and 1, representing (-1)^k in the boundary map formula
+            for k in range(cube_dim):  # loop through all dimensions of the chosen cube
+                mask = 1 << k  # mask to differentiate between the positive and negative face in this dim
+                face_0 = []  # negative face
+                face_1 = []  # positive face
+                for h in range(len(singular_cubes[cube_dim][j])):  # loop through all vertices of the cube
+                    # using bit operations, we append the vertex either to the positive or negative face
+                    if h & mask == 0:
+                        face_0.append(singular_cubes[cube_dim][j][h])
+                    else:
+                        face_1.append(singular_cubes[cube_dim][j][h])
+
+                for cube_index in range(len(singular_cubes[cube_dim - 1])):  # loop through all cube of dim cube_dim - 1
+                    # when we find the positive (or negative) face, we can write it into the current matrix column
+                    # see cubical boundary formula for more information
+                    # it can happen, that a positive/negative face is NOT found. this happens, when it is degenerate
+                    # in this case, we just ignore it, since it is 0 in our chain complex (see definition)
+                    if face_0 == singular_cubes[cube_dim - 1][cube_index]:
+                        matrix_column[cube_index] += summand
+                    if face_1 == singular_cubes[cube_dim - 1][cube_index]:
+                        matrix_column[cube_index] -= summand
+                summand = -summand  # change summand sign
+            face_matrix[j] = matrix_column  # add column to matrix
+        result[cube_dim] = face_matrix
+
+    return result
 
 
 class FaceMapGeneratorScheduler:
@@ -13,22 +53,44 @@ class FaceMapGeneratorScheduler:
         self.thread_number = thread_number
 
     def run(self):
+        total_number = 0
+        for dim in range(len(self.singular_cubes)):
+            total_number += len(self.singular_cubes[dim])
 
-        with ThreadPoolExecutor(max_workers=self.thread_number) as executor:
-            futures = []
-            numbers_per_thread = (self.graph.order() // self.thread_number)+1
-
-            for i in range(self.thread_number):
-                start_index = i*numbers_per_thread
-                stop_index = max((i+1)*numbers_per_thread, self.graph.order())
-                futures.append(
-                    executor.submit(generate_singular_cubes, adjacency_graph=adjacency_graph, max_dim=self.max_dim,
-                                    start_options=list(range(start_index, stop_index))))
-                if stop_index == self.graph.order():
+        numbers_per_thread = (total_number // self.thread_number) + 1
+        index = 0
+        dim = 0
+        args = []
+        for i in range(self.thread_number):
+            start_index = index
+            start_dim = dim
+            number = numbers_per_thread
+            while index + number > len(self.singular_cubes[dim]):
+                number += index - len(self.singular_cubes[dim])
+                dim += 1
+                if dim >= len(self.singular_cubes):
                     break
 
-            for future in futures:
-                future_result = future.result()
-                for i in range(self.max_dim + 1):
-                    result[i] = result[i] + future_result[i]
-        return result
+            stop_dim = dim if dim < len(self.singular_cubes) else len(self.singular_cubes) - 1
+            stop_index = index + number if dim < len(self.singular_cubes) else len(self.singular_cubes[-1])
+            args.append([self.singular_cubes, [start_dim, start_index], [stop_dim, stop_index]])
+            if dim >= len(self.singular_cubes):
+                break
+
+        with Pool(self.thread_number) as p:
+            p_results = p.starmap(generate_face_maps, args)
+
+        face_maps = dict()  # result dictionary
+        for p_result in p_results:
+            for dim in range(len(p_result)):
+                if p_result[dim] is not None:
+                    if dim in face_maps.keys():
+                        face_maps[dim].extend(p_result[dim])
+                    else:
+                        face_maps[dim] = p_result[dim]
+
+        for dim in face_maps.keys():
+            face_maps[dim] = Matrix(ZZ, face_maps[dim]).transpose()
+
+        face_maps[0] = Matrix(0, len(self.singular_cubes[0]))
+        return face_maps
--- test2.py
+++ test2.py
@@ -3,16 +3,23 @@ import time
 from sage.all import *
 from sage.graphs.graph_generators import GraphGenerators
 import sage_utility
+from persistence_calculator.FaceMapGenerator import FaceMapGeneratorScheduler
 
 from persistence_calculator.SingularCubeGenerator import SingularCubeGeneratorScheduler
 
 G = sage.graphs.graph_generators.GraphGenerators.CubeGraph(7)
 stamp = time.time()
-cubes1 = sage_utility.find_singular_non_degenerate_cubes(G, 8)
+cubes1 = sage_utility.find_singular_non_degenerate_cubes(G, 2)
 print(time.time() - stamp)
 stamp = time.time()
 scheduler = SingularCubeGeneratorScheduler(G, 2, 4)
 cubes2 = scheduler.run()
 print(time.time() - stamp)
-print(cubes1)
-print(cubes2)
+print("---")
+stamp = time.time()
+face_maps1 = sage_utility.create_face_maps(cubes1)
+print(time.time() - stamp)
+stamp = time.time()
+scheduler = FaceMapGeneratorScheduler(cubes2, 4)
+face_maps2 = scheduler.run()
+print(time.time() - stamp)
